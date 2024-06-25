c907957e70f53e8785ed6fac1c2095538e40963f
Jose Velasquez <163069850+Nachovels@users.noreply.github.com>
2024-06-25 11:56:20 -0400

    Add files via upload

---
+++ SimulaciónPrueba3.py
@@ -0,0 +1,14 @@
+while True:
+    print("***MENU***\n1. Registrar pedido\n2. Listar todos los pedidos\n3. Imprimir hoja de ruta\n4. Salir del programa")
+    op = input("Seleccione opción: ")
+    if op == "1": 
+        print("Ingresando a opción 1...")
+    elif op == "2": 
+        print("Ingresando a opción 2...")
+    elif op == "3": 
+        print("Ingresando a opción 3...")
+    elif op == "4": 
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Ingrese una opción válida (1-4)")
